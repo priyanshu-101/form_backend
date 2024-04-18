4aff82632904b8e9af3a19f7ad9f0335c59f4da5
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2024-04-18 14:35:54 +0530

    final commit

--- model/User.js
+++ model/User.js
@@ -15,6 +15,6 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-},{timestamps: true});
+},{timestamps: true}) ;
 
 module.exports = mongoose.model('User', UserSchema);
